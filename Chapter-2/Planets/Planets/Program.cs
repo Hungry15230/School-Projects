1a6afa1e87aa21da0d5906a5f1b80399c5c3cb6b
Hungry15230 <83186716+Hungry15230@users.noreply.github.com>
2024-10-23 14:15:43 -0400

    Add files via upload

---
+++ Chapter-2/Planets/Planets/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Planets
+{
+    class Program
+    {
+        enum Planets
+        {
+            MERCURY=1, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Desired planet number: ");
+            int planetNum = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"The planet in that position is: {(Planets)planetNum}");
+        }
+    }
+}
