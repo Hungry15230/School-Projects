38ea8f483e4dec0d22170c3bf73c6ef0b30e247d
Hungry15230 <83186716+Hungry15230@users.noreply.github.com>
2024-10-23 14:26:41 -0400

    Add files via upload

---
+++ Chapter-5/DailyTemps/DailyTemps/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DailyTemps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* List experiments
+            List<int> myList = [1, 2, 3];
+            Console.WriteLine(myList.Contains(1));
+            */
+            int response = 5;
+            int totalTemp = 0;
+            int totalResponses = 0;
+            while (response != 0)
+            {
+                Console.Write("Enter a daily high temperature between -20F and 130F, 0 to quit: ");
+                response = Convert.ToInt32(Console.ReadLine());
+                if (response == 0) { break; }
+                if (response <= 130 && response >= -20)
+                {
+                    totalResponses++;
+                    totalTemp += response;
+                    Console.WriteLine("Temp added");
+                } else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            Console.WriteLine("Total temperatures: " + totalResponses);
+            Console.WriteLine("Average temperature: " + totalTemp/totalResponses);
+        }
+    }
+}
