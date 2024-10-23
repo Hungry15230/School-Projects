f3d620910bf3bc6394793afd38e75a89af7f925b
Hungry15230 <83186716+Hungry15230@users.noreply.github.com>
2024-10-23 14:14:52 -0400

    Add files via upload

---
+++ Chapter-2/MakeChange/MakeChange/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MakeChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dollars = 113;
+            int startingDollars = dollars;
+            int twenties = dollars / 20;
+            dollars = dollars % 20;
+            int tens = dollars / 10;
+            dollars = dollars % 10;
+            int fives = dollars / 5;
+            dollars = dollars % 5;
+            int ones = dollars / 1;
+            dollars = dollars % 1;
+            Console.WriteLine($"{startingDollars} is worth {twenties} twenties, {tens} tens, {fives} fives, and {ones} ones.");
+        }
+    }
+}
