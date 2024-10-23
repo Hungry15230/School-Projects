430f658ca4b8561cfffeee5867f70211263e030e
Hungry15230 <83186716+Hungry15230@users.noreply.github.com>
2024-10-23 14:28:20 -0400

    Add files via upload

---
+++ Chapter-5/SumInts/SumInts/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SumInts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int number = 0;
+            while (number != 999)
+            {
+                Console.Write("Enter a number: ");
+                sum += number;
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine($"Sum of numbers entered, not including 999: {sum}");
+        }
+    }
+}
