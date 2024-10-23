ecd92fd7d039e589c452b810397b73d78ceaa65c
Hungry15230 <83186716+Hungry15230@users.noreply.github.com>
2024-10-23 14:12:23 -0400

    Add files via upload

---
+++ Chapter-2/DemoVariables3/DemoVariables3/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace DemoVariables3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int comparisonValue = 4;
+            bool isSixMore = 6 > comparisonValue;
+            WriteLine("When comparisonValue is {0}, isSixMore is{1}", comparisonValue, isSixMore);
+            comparisonValue = 35;
+            isSixMore = 6 > comparisonValue;
+            WriteLine("When comparisonValue is {0}, isSixMore is {1}", comparisonValue, isSixMore);
+        }
+    }
+}
