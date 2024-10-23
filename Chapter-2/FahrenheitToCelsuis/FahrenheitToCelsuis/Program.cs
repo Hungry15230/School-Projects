412fd7a9a3578c3296e99ac4aabfe724840a3544
Hungry15230 <83186716+Hungry15230@users.noreply.github.com>
2024-10-23 14:14:02 -0400

    Add files via upload

---
+++ Chapter-2/FahrenheitToCelsuis/FahrenheitToCelsuis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FahrenheitToCelsuis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Temperature in Fahrenheit: ");
+            double tempInF = Convert.ToDouble(Console.ReadLine());
+            double tempInC = (tempInF - 32)*0.5;
+            Console.WriteLine($"Is equivalent to {tempInC:F1} celsius.");
+        }
+    }
+}
