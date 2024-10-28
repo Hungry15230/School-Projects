b5e5f932a8de0fca2768156497d650478287e423
Hungry15230 <hungry15230@gmail.com>
2024-10-28 14:20:08 -0400

    Sync With VSCode

--- Chapter-3/InchesToCentimetersGUI/InchesToCentimetersGUI/obj/Debug/net8.0-windows/InchesToCentimetersGUI.AssemblyInfo.cs
+++ Chapter-3/InchesToCentimetersGUI/InchesToCentimetersGUI/obj/Debug/net8.0-windows/InchesToCentimetersGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InchesToCentimetersGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64f27ecb9f149d74265a0e3353676cc5a9ed4f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("InchesToCentimetersGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InchesToCentimetersGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
