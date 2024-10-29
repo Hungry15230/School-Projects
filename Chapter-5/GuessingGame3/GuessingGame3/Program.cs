80e130c089b49922b18865c8b315c46c5789efd8
Hungry15230 <Hungry15230@gmail.com>
2024-10-28 20:56:00 -0400

    Finished GuessingGame3

--- Chapter-5/GuessingGame3/GuessingGame3/Program.cs
+++ Chapter-5/GuessingGame3/GuessingGame3/Program.cs
@@ -4,16 +4,8 @@
     {
         static void Main(string[] args)
         {
-            /*
-            Modify the GuessingGame2 program to create a program called GuessingGame3
-            in which the player is criticized for making a “dumb” guess. For example, if the player
-            guesses that the random number is 4 and is told that the guess is too low, and then the
-            player subsequently makes a guess lower than 4, display a message that the user should
-            have known not to make such a low guess
-            */
             Random randomNumberGen = new Random();
             int randomNumber = randomNumberGen.Next(1, 11);
-
             int obviouslyLowerThan = 11;
             int obviouslyHigherThan = 0;
             while (true)
@@ -21,7 +13,6 @@
                 Console.Write("Enter a number 1-11: ");
                 string userInput = Console.ReadLine() ?? "";
                 int userNumber = Convert.ToInt16(userInput);
-
                 if (randomNumber == userNumber)
                 {
                     Console.WriteLine("Correct number guessed.");
