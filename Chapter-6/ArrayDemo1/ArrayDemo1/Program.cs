c551e622b1128923bd6fc58bf86ebb042df0f23d
Hungry15230 <Hungry15230@gmail.com>
2024-10-29 14:44:48 -0400

    Added ArrayDemo1

---
+++ Chapter-6/ArrayDemo1/ArrayDemo1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ArrayDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] payRate = { 13.00, 17.35, 19.12, 22.45 };
+            for(int x = 0; x < payRate.Length; ++x)
+            {
+                Console.WriteLine($"Pay rate {x} is {payRate[x].ToString("C")}");
+            }
+        }
+    }
+}
