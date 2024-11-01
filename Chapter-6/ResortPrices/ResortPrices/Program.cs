8431e3942ec16ec07e458e6925028957001edc8e
Hungry15230 <Hungry15230@gmail.com>
2024-11-01 14:42:39 -0400

    Added ResortPrices

---
+++ Chapter-6/ResortPrices/ResortPrices/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ResortPrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many days will you be staying with Carefree Resorts? ");
+            int daysStaying = Convert.ToInt32(Console.ReadLine());
+            int[] dayLowRanges = { 1, 3, 5, 8 };
+            int[] nightlyRates = { 200, 180, 160, 145};
+            int sub = dayLowRanges.Length - 1;
+            while (sub >= 0 && daysStaying < dayLowRanges[sub]) --sub;
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine($"Days staying: {daysStaying}");
+            Console.WriteLine($"Price/Night:  {nightlyRates[sub]}$");
+            Console.WriteLine($"Total cost:   {daysStaying * nightlyRates[sub]}$");
+        }
+    }
+}
