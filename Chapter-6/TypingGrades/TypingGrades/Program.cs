52cdf7fbf46e7aa44a3a8081fc4e74c6166dc553
Hungry15230 <Hungry15230@gmail.com>
2024-10-31 21:49:14 -0400

    Added TypingGrades

---
+++ Chapter-6/TypingGrades/TypingGrades/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TypingGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] wordLowRanges = { 0, 16, 31, 51, 76 };
+            char[] grades = { 'F', 'D', 'C', 'B', 'A' };
+            Console.Write("Words per minute: ");
+            int wordsPerMin = Convert.ToInt32(Console.ReadLine());
+            int sub = wordLowRanges.Length - 1;
+            while (sub >= 0 && wordsPerMin < wordLowRanges[sub]) --sub;
+            Console.WriteLine($"Grade: {grades[sub]}");
+        }
+    }
+}
