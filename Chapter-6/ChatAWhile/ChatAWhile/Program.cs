802e0b0d005100f9e17a279c02d2b2cbdb0bf083
Hungry15230 <Hungry15230@gmail.com>
2024-10-31 21:41:01 -0400

    Added ChatAWhile

---
+++ Chapter-6/ChatAWhile/ChatAWhile/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ChatAWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] areaCodes = { 262, 414, 608, 715, 815, 920 };
+            double[] payRates = { 0.07, 0.10, 0.05, 0.16, 0.24, 0.14 };
+            Console.Write("Area code: ");
+            int areaCode = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Call time, in minutes: ");
+            int callTime = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"Total call cost: {payRates[Array.BinarySearch(areaCodes,areaCode)]*callTime}$");
+        }
+    }
+}
