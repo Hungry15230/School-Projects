36a331d4f300a2851efbbb557e59380d778e3d1a
Hungry15230 <Hungry15230@gmail.com>
2024-11-08 14:43:03 -0500

    Added GuessAWord

---
+++ Chapter-6/GuessAWord/GuessAWord/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace GuessAWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string currentWord;
+            string knownLetters = "";
+            string[] wordOptions = { "programming", "marathon", "magical", "hypothalamus", "mordor", "coconut", "basalt", "volcano" };
+            List<int> indexesOfLetters = new List<int>();
+            currentWord = wordOptions[random.Next(0, 8)];
+            for (int i = 0; i < currentWord.Length; i++) { knownLetters += "_"; }
+            while (true)
+            {
+                Console.Write($"Word to guess: ");
+                Console.WriteLine(knownLetters);
+                Console.Write("Guess a letter: ");
+                char guessedLetter = Convert.ToChar(Console.ReadLine() ?? "");
+                if (currentWord.Contains(guessedLetter))
+                {
+                    indexesOfLetters.Clear();
+                    for (int i = 0; i < currentWord.Length; i++)
+                    {
+                        if (currentWord[i] == guessedLetter) { indexesOfLetters.Add(i); }
+                    }
+                    for (int i = 0; i < indexesOfLetters.Count; i++)
+                    {
+                        knownLetters = knownLetters.Remove(indexesOfLetters[i], 1).Insert(indexesOfLetters[i], Convert.ToString(guessedLetter));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Letter");
+                }
+                if (!knownLetters.Contains("_")) { break; }
+            }
+            Console.WriteLine(knownLetters);
+            Console.WriteLine("Congrats!");
+        }
+    }
+}
