e5e15d05cf7398a37e9c8879fedef0e92cc42c7f
Hungry15230 <Hungry15230@gmail.com>
2024-11-08 22:10:12 -0500

    Added DemoLogo

---
+++ Chapter-7/DemoLogo/DemoLogo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoLogo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Our company is ");
+            DisplayCompanyLogo();
+        }
+        private static void DisplayCompanyLogo()
+        {
+            Console.WriteLine("See Sharp Optical");
+            Console.WriteLine("We prize your eyes");
+        }
+    }
+}
