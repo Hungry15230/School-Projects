4e379389442b8cacef7864f1bd4996c5c272c231
Hungry15230 <Hungry15230@gmail.com>
2024-11-11 14:45:07 -0500

    Added ConvertMilesToKilometers

---
+++ Chapter-7/ConvertMilesToKilometers/ConvertMilesToKilometers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConvertMilesToKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of miles: ");
+            double miles = Convert.ToDouble(Console.ReadLine());
+            double kilometers = Conversion(miles);
+            Console.WriteLine($"Kilometers: {kilometers}");
+
+        }
+        static double Conversion(double miles)
+        {
+            return miles * 1.60934;
+        }
+    }
+}
