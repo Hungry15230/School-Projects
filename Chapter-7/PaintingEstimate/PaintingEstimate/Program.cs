9713cab61dd2b9c8987542547cd0c4393202b0cb
Hungry15230 <Hungry15230@gmail.com>
2024-11-11 14:45:52 -0500

    Added PaintingEstimate

---
+++ Chapter-7/PaintingEstimate/PaintingEstimate/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PaintingEstimate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int costPerSqFt = 6;
+            int roomHeight = 9;
+            Console.Write("Length of room: ");
+            int roomLength = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Width of room: ");
+            int roomWidth = Convert.ToInt32(Console.ReadLine());
+            int totalCost = CalcPaintCost(costPerSqFt, roomHeight, roomLength, roomWidth);
+            Console.WriteLine($"Total Cost: {totalCost.ToString("C")}$");
+        }
+        static int CalcPaintCost(int costPerSqFt, int roomHeight, int roomLength, int roomWidth)
+        {
+            return ((roomHeight*roomLength*2)+(roomHeight*roomWidth*2))*costPerSqFt;
+        }
+    }
+}
