7b3c49fbd8c60dcab15cd175509946a4b8a3416e
Hungry15230 <Hungry15230@gmail.com>
2024-11-11 14:45:40 -0500

    Added SalesLetter

---
+++ Chapter-7/SalesLetter/SalesLetter/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SalesLetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dear person,");
+            DisplayContactInfo();
+            Console.WriteLine("This is a letter.");
+            DisplayContactInfo();
+            Console.WriteLine("And this is spam.");
+            DisplayContactInfo();
+        }
+
+        private static void DisplayContactInfo()
+        {
+            Console.WriteLine("Phone Number:   999-999-9999");
+            Console.WriteLine("Email Address:  nowhere@gmail.com");
+            Console.WriteLine("Street Address: N/A");
+        }
+    }
+}
