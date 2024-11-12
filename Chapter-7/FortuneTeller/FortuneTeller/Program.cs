51e2334bb60f4a6ca45d0b9cb63ce967bb0c62be
Hungry15230 <Hungry15230@gmail.com>
2024-11-12 14:27:04 -0500

    Added FortuneTeller

---
+++ Chapter-7/FortuneTeller/FortuneTeller/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FortuneTeller
+{
+    internal class Program
+    {
+        static void DisplayFortunes(string fortune1, string fortune2)
+        {
+            Console.WriteLine(fortune1);
+            Console.WriteLine(fortune2);
+        }
+        static void Main(string[] args)
+        {
+            string[] fortunes = {"I see misfortune in your future.", "I see dissapointment in your future.", "I see despair in your future.",
+                                 "I see people in your future.", "I see buildings in your future.", "I see mercantalism in your future."};
+            Random random = new Random();
+            DisplayFortunes(fortunes[random.Next(0, 6)], fortunes[random.Next(0, 6)]);
+        }
+    }
+}
