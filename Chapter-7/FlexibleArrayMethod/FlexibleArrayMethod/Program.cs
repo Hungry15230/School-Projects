32f3b4d143f06f6bbdbb41b3dc3682c8f1eb7b73
Hungry15230 <Hungry15230@gmail.com>
2024-11-12 14:42:27 -0500

    Added FlexibleArrayMethod

---
+++ Chapter-7/FlexibleArrayMethod/FlexibleArrayMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FlexibleArrayMethod
+{
+    internal class Program
+    {
+        static void DisplayArray(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5};
+            int[] array2 = { 6, 7, 8 };
+            int[] array3 = { 10, 11, 12, 13, 14, 15};
+            DisplayArray(array1);
+            DisplayArray(array2);
+            DisplayArray(array3);
+        }
+    }
+}
