474ecc4d0a82458f6708222fa82e86c2e9e21128
Hungry15230 <Hungry15230@gmail.com>
2024-11-12 14:26:34 -0500

    Added AdmissionModularized

---
+++ Chapter-7/AdmissionModularized/AdmissionModularized/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AdmissionModularized
+{
+    internal class Program
+    {
+        static string TestAcceptance(double gpa, int admissionTestScore)
+        {
+            if ((admissionTestScore >= 60 && gpa >= 3.0) || (admissionTestScore >= 80 && gpa < 3.0))
+            {
+                return "Accept";
+            }
+            else
+            {
+                return "Reject";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("High School GPA: ");
+            double gpa = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Admission Test Score: ");
+            int admissionTestScore = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(TestAcceptance(gpa, admissionTestScore));
+        }
+    }
+}
