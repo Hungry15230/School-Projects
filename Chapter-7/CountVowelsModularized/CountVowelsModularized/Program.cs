33c1845a937cb7a95422f8f20d469740d7e6c445
Hungry15230 <Hungry15230@gmail.com>
2024-11-12 14:34:52 -0500

    Added CountVowelsModularized

---
+++ Chapter-7/CountVowelsModularized/CountVowelsModularized/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CountVowelsModularized
+{
+    internal class Program
+    {
+        static int CountVowels(string userInput)
+        {
+            int vowels = 0;
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                char charToEval = Char.ToLower(userInput[i]);
+                if (charToEval == 'a' || charToEval == 'e' || charToEval == 'i' || charToEval == 'o' || charToEval == 'u') { vowels++; }
+            }
+            return vowels;
+
+        }
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine() ?? "";
+            Console.WriteLine(CountVowels(userInput) + " vowels.");
+        }
+    }
+}
