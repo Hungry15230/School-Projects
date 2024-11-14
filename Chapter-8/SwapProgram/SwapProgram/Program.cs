6970219a9d939f48690cf9ecd7f3991965d7d52d
Hungry15230 <Hungry15230@gmail.com>
2024-11-14 14:18:51 -0500

    Added SwapProgram

---
+++ Chapter-8/SwapProgram/SwapProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SwapProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 34, second = 712;
+            Console.Write($"Before swap first is {first}");
+            Console.WriteLine($" and second is {second}");
+            Swap(ref first, ref second);
+            Console.Write($"After swap first is {first}");
+            Console.WriteLine($" and second is {second}");
+        }
+        private static void Swap(ref int one, ref int two)
+        {
+            int temp;
+            temp = one;
+            one = two;
+            two = temp;
+        }
+    }
+}
