620eafc2fe86ffe88f2142dcf758f6155f9da694
Hungry15230 <Hungry15230@gmail.com>
2024-11-14 14:12:14 -0500

    Added TryParseExample

---
+++ Chapter-8/TryParseExample/TryParseExample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TryParseExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string entryString;
+            int score;
+            Console.Write("Enter your test score: ");
+            entryString = Console.ReadLine() ?? "";
+            int.TryParse(entryString, out score);
+            Console.WriteLine($"You entered {score}");
+        }
+    }
+}
