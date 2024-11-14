d3ecef211dd6b66035c4a6b61bded193ff8d9ec0
Hungry15230 <Hungry15230@gmail.com>
2024-11-14 18:54:40 -0500

    Added personal project TextAdventure

---
+++ TextAdventure-v1.00/TextAdventure-v1.00/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace TextAdventure_v1._00
+{
+    internal class Program
+    {
+        static bool ParseAndExecute(string input)
+        {
+            char[] delimiters = { ' ', '\n' };
+            string[] tokens = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            string? verb = tokens.Length > 0 ? tokens[0] : null;
+            string? noun = tokens.Length > 1 ? tokens[1] : null;
+            if (verb != null)
+            {
+                verb = verb.ToLower();
+                if (String.Compare(verb, "quit") == 0)
+                {
+                    return false;
+                }
+                else if (String.Compare(verb, "look") == 0)
+                {
+                    Console.WriteLine("It is very dark in here.");
+                }
+                else if (String.Compare(verb, "go") == 0)
+                {
+                    Console.WriteLine("It's too dark to go anywhere.");
+                }
+                else
+                {
+                    Console.WriteLine("I don't know how to '{0}'.", verb);
+                }
+            }
+            return true;
+        }
+
+        static bool GetInput(out string input)
+        {
+            Console.Write("> ");
+            input = Console.ReadLine() ?? "";
+            return input != null && input.Length > 0;
+        }
+
+        static void GameLoop()
+        {
+            string input;
+            while (GetInput(out input) && ParseAndExecute(input)) { }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Little Cave Adventure.");
+
+            while (true)
+            {
+                GameLoop();
+                Console.WriteLine("You failed. Play again? (y/n)");
+                string userInput = Console.ReadLine() ?? "";
+                if (userInput == "y") { GameLoop(); }
+                else { break; }
+            }
+
+            Console.WriteLine("Bye!");
+        }
+    }
+}
