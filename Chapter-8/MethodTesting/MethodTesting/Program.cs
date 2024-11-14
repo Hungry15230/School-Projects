b1671d41c2fba9203ff6ac8f9e945871eb185178
Hungry15230 <Hungry15230@gmail.com>
2024-11-14 14:11:59 -0500

    Added MethodTesting

---
+++ Chapter-8/MethodTesting/MethodTesting/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MethodTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            DisplayReferenceParameter(ref x);
+            Console.WriteLine($"In Main(), x is {x}");
+        }
+
+        private static void DisplayReferenceParameter(ref int number)
+        {
+            number = 888;
+            Console.WriteLine($"In method, number is {number}");
+        }
+    }
+}
