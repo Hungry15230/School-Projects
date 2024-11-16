bb44198db44fef093471545d7b7b643d60ae9768
Hungry15230 <Hungry15230@gmail.com>
2024-11-15 20:41:30 -0500

    Added Reverse4

---
+++ Chapter-8/Reverse4/Reverse4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Reverse4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstInt = 3;
+            int secondInt = 6;
+            int thirdInt = 10;
+            int fourthInt = 14;
+            Console.WriteLine($"{firstInt}, {secondInt}, {thirdInt}, {fourthInt}");
+            Reverse(ref firstInt, ref secondInt, ref thirdInt, ref fourthInt);
+            Console.WriteLine($"{firstInt}, {secondInt}, {thirdInt}, {fourthInt}");
+        }
+
+        private static void Reverse(ref int first, ref int second, ref int third, ref int fourth)
+        {
+            (fourth, first) = (first, fourth);
+            (third, second) = (second, third);
+        }
+    }
+}
