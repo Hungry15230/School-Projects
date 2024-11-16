e6006995362eaed58fa4a79eabcaa6ded7bd7e64
Hungry15230 <Hungry15230@gmail.com>
2024-11-15 20:41:15 -0500

    Added Reverse3

---
+++ Chapter-8/Reverse3/Reverse3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Reverse3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstInt = 3;
+            int middleInt = 6;
+            int lastInt = 10;
+            Console.WriteLine($"{firstInt}, {middleInt}, {lastInt}");
+            Reverse(ref firstInt, ref lastInt);
+            Console.WriteLine($"{firstInt}, {middleInt}, {lastInt}");
+        }
+
+        private static void Reverse(ref int first, ref int last)
+        {
+            (last, first) = (first, last);
+        }
+    }
+}
