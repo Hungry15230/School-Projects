247cadf1c72ffed64d256cb9e5fc001b7f46f18a
Hungry15230 <Hungry15230@gmail.com>
2024-11-18 14:08:40 -0500

    Updated Auction

--- Chapter-8/Auction/Auction/obj/Debug/net6.0/Auction.AssemblyInfo.cs
+++ Chapter-8/Auction/Auction/obj/Debug/net6.0/Auction.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7c1279c66c43b8ce7a3d097daa7213668c5a849")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
