86bc559af80c047c9f093e4260402fd45e37893d
Hungry15230 <Hungry15230@gmail.com>
2024-11-20 14:40:11 -0500

    Added InputMethodDemo2

---
+++ Chapter-8/InputMethodDemo2/InputMethodDemo2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace InputMethodDemo2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int first, second;
+            InputMethod(out first, out second);
+            Console.WriteLine($"After InputMethod first is {first} and second is {second}");
+        }
+        private static void InputMethod(out int one, out int two)
+        {
+            one = DataEntry("first");
+            two = DataEntry("second");
+        }
+        static int DataEntry(string myString)
+        {
+            Console.Write($"Enter {myString} integer: ");
+            return Convert.ToInt16(Console.ReadLine() ?? "");
+        }
+    }
+}
