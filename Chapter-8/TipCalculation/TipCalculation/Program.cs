f3d69304ba707e6eb47b32a0d5fc589e730180b5
Hungry15230 <Hungry15230@gmail.com>
2024-11-20 14:26:38 -0500

    Added TipCalculation

---
+++ Chapter-8/TipCalculation/TipCalculation/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TipCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateTip(10, 0.1);
+            Console.WriteLine("-------------------");
+            CalculateTip(10, 10);
+        }
+        static void CalculateTip(double mealPrice, double tip)
+        {
+            Console.WriteLine($"Meal Price:  {mealPrice.ToString("C")}");
+            Console.WriteLine($"Tip Percent: {tip * 100}%");
+            Console.WriteLine($"Tip Amount:  {(mealPrice * (1 + tip)).ToString("C")}");
+        }
+        static void CalculateTip(double mealPrice, int tip)
+        {
+            Console.WriteLine($"Meal Price:  {mealPrice.ToString("C")}");
+            Console.WriteLine($"Tip Percent: {tip}%");
+            Console.WriteLine($"Tip Amount:  {(mealPrice * (1 + (Convert.ToDouble(tip) / 100))).ToString("C")}");
+        }
+    }
+}
