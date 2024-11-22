d31d7a1c91d9ca6179d7c622840e5edfc7445fbf
Hungry15230 <Hungry15230@gmail.com>
2024-11-22 14:46:17 -0500

    Added SortWords

---
+++ Chapter-8/SortWords/SortWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Immutable;
+
+namespace SortWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sort("Apple");
+            Console.WriteLine("----------");
+            Sort("Apple", "Zebra");
+            Console.WriteLine("----------");
+            Sort("Apple", "Zebra", "Coconut", "Bannana", "Dog");
+            Console.WriteLine("----------");
+            Sort("Apple", "Zebra", "Coconut", "Bannana", "Dog",
+                 "Cracker", "Lake", "Mountain", "Sea", "Ocean");
+        }
+
+        static void Sort(params string[] args)
+        {
+            Array.Sort(args);
+            foreach (string word in args)
+                Console.WriteLine(word);
+        }
+    }
+}
