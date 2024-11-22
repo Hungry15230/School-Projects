cb6fdfee279ddaf28da9518935cdc97e2f5cfb85
Hungry15230 <Hungry15230@gmail.com>
2024-11-22 14:05:07 -0500

    Updated Averages

---
+++ Chapter-8/Averages/Averages/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Averages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayAverage(1);
+            Console.WriteLine("-----");
+            DisplayAverage(1, 2);
+            Console.WriteLine("-----");
+            DisplayAverage(1, 2, 3);
+            Console.WriteLine("-----");
+            DisplayAverage([1, 2, 3, 4, 5]);
+        }
+        static void DisplayAverage(params int[] args)
+        {
+            foreach (int i in args)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
