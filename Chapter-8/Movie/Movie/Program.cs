be9fda96576653e750effac40e6884b9a8a9015a
Hungry15230 <Hungry15230@gmail.com>
2024-11-25 14:42:55 -0500

    Added Movie

---
+++ Chapter-8/Movie/Movie/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Movie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMovie("Return of the King", 180);
+            DisplayMovie("The Two Towers");
+        }
+
+        static void DisplayMovie(string name, int minutes = 90)
+        {
+            Console.WriteLine($"Movie name: {name}.");
+            Console.WriteLine($"Movie time: {minutes} minutes.");
+        }
+    }
+}
