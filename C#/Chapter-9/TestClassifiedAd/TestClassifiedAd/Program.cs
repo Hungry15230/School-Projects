b54ede3a72ae0e0efff988b451d148e21349f237
Hungry15230 <Hungry15230@gmail.com>
2024-12-14 21:44:40 -0500

    Add TestClassifiedAd

---
+++ C#/Chapter-9/TestClassifiedAd/TestClassifiedAd/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Create an application named TestClassifiedAd that instantiates and displays at least
+    two ClassifiedAd objects. A ClassifiedAd has fields for a category (for example,
+    Used Cars), a number of words, and a price. Include properties that contain get and
+    set accessors for the category and number of words, but only a get accessor for the
+    price. The price is calculated at nine cents per word.
+*/
+
+namespace TestClassifiedAd
+{
+    class ClassifiedAd
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
