65511036f0bba9af03a93b7db705bdc92f002e08
Hungry15230 <Hungry15230@gmail.com>
2025-01-20 09:55:31 -0500

    Create TestProgram4

---
+++ C++/Chapter-0/TestProgram4/test4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x {};
+    int y {};
+    int z {};
+    cout << "X will be: ";
+    cin >> x;
+    cout << "X is: " << x << "\n";
+
+    cout << "Y and Z will be: ";
+    cin >> y >> z;
+    cout << "Y is " << y << " and Z is " << z << "\n";
+
+    cout << "This is test4.cpp\n";
+    return 0;
+}
