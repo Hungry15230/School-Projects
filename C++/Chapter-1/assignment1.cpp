9976ddefc3692e3528ecc7630129e4fc64eb9a99
Hungry15230 <Hungry15230@gmail.com>
2025-02-25 14:49:14 -0500

    Added assignments

---
+++ C++/Chapter-1/assignment1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Walk forward one complete step.";
+    }
+    cout << "Step over ginger.";
+    cout << "Until in front of chair...";
+    cout << "Walk forward one complete step.";
+    cout << "Turn left 90 degrees.";
+    cout << "Turn left 90 degrees.";
+    cout << "Sit down in the chair.";
+    return 0;
+}
