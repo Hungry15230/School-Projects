3ef35685860044fd04ab270e084368843538ec4d
Hungry15230 <Hungry15230@gmail.com>
2025-02-27 14:23:40 -0500

    Updated assignment2.cpp

--- C++/Chapter-1/assignment2.cpp
+++ C++/Chapter-1/assignment2.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main() {
-    
+    int age;
+    double salesAmount;
+    double discount;
+    double amountDue;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter the sales amount: ";
+    cin >> salesAmount;
+    if (age < 55) {
+        discount = salesAmount * 0.10;
+    } else {
+        discount = salesAmount * 0.15;
+    }
+    amountDue = salesAmount - discount;
+    cout << "Amount due: " << amountDue;
 }
