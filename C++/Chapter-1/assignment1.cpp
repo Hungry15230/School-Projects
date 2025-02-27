200a0b10ed3f2ffc80136e0c3f90d058cb6358eb
Hungry15230 <Hungry15230@gmail.com>
2025-02-27 14:36:38 -0500

    Update assignment1.cpp

--- C++/Chapter-1/assignment1.cpp
+++ C++/Chapter-1/assignment1.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
     for (int i = 0; i < 4; i++)
     {
-        cout << "Walk forward one complete step.";
+        cout << "Walk forward one complete step.\n";
     }
-    cout << "Step over ginger.";
-    cout << "Until in front of chair...";
-    cout << "Walk forward one complete step.";
-    cout << "Turn left 90 degrees.";
-    cout << "Turn left 90 degrees.";
-    cout << "Sit down in the chair.";
+    cout << "Step over ginger.\n";
+    cout << "Until in front of chair...\n";
+    cout << "Walk forward one complete step.\n";
+    cout << "Turn left 90 degrees.\n";
+    cout << "Turn left 90 degrees.\n";
+    cout << "Sit down in the chair.\n";
     return 0;
 }
