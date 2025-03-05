b8e0b5da3c1c066b6cfb5a0b3d866e03123a4ce0
Hungry15230 <Hungry15230@gmail.com>
2025-03-05 14:47:11 -0500

    Working on a personal RPG

---
+++ C++/Personal-Projects/RPG.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+Physical statistics:
+    -Strength - Damage
+    -Constitution - Health
+    -Dexterity - Agility
+Mental statistics:
+    -Intelligence - XP Gain
+    -Wisdom - Magic
+    -Charisma - Lower NPC Prices
+*/
+
+
+
+int main()
