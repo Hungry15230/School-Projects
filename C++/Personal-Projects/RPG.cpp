9eb697aee25b2171b55bb560a6b874eeb37bcd3f
Hungry15230 <Hungry15230@gmail.com>
2025-03-06 14:48:16 -0500

    Update RPG.cpp

--- C++/Personal-Projects/RPG.cpp
+++ C++/Personal-Projects/RPG.cpp
@@ -12,6 +12,43 @@ Mental statistics:
     -Charisma - Lower NPC Prices
 */
 
+class Player {
+    public:
+        int STR, CON, DEX, INT, WIS, CHA, LVL;
+        double CD, CC, HP, DEF, MONEY, XP, DMG;
+        string ORIGIN, NAME;
+        Player(int STR, int CON, int DEX, int INT, int WIS, int CHA,
+               double CD, double CC, double HP, double DEF, double DMG,
+               string ORIGIN, string NAME, double MONEY, int LVL, double XP) {
+            STR = STR;
+            CON = CON;
+            DEX = DEX;
+            INT = INT;
+            WIS = WIS;
+            CHA = CHA;
+
+            CD = CD;
+            CC = CC;
+
+            HP = HP;
+            DEF = DEF;
+            DMG = DMG;
+            ORIGIN = ORIGIN;
+            NAME = NAME;
+            MONEY = MONEY;
+            LVL = LVL;
+            XP = XP;
+        }
+    void displayStats() {
+        cout << "";
+    }
+};
+
+int main(){
+    //Stats: str, con, dex, int, wis, cha, cd, cc, hp, arm, dmg, origin, name, money, lvl, xp
+    Player player(1, 1, 1, 1, 1, 1, 1.10, 1, 100, 0, 1, "", "", 0, 1, 0);
 
 
-int main()
+
+    return 0;
+}
