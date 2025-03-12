bafa91ebebd47e9f89dabeca56be123958da37b6
Hungry15230 <Hungry15230@gmail.com>
2025-03-12 14:34:58 -0400

    Update RPG.cpp

--- C++/Personal-Projects/RPG.cpp
+++ C++/Personal-Projects/RPG.cpp
@@ -15,40 +15,78 @@ Mental statistics:
 class Player {
     public:
         int STR, CON, DEX, INT, WIS, CHA, LVL;
-        double CD, CC, HP, DEF, MONEY, XP, DMG;
+        double CD, CC, HP, DEF, MONEY, XP, DMG, HP_MAX;
         string ORIGIN, NAME;
-        Player(int STR, int CON, int DEX, int INT, int WIS, int CHA,
-               double CD, double CC, double HP, double DEF, double DMG,
-               string ORIGIN, string NAME, double MONEY, int LVL, double XP) {
-            STR = STR;
-            CON = CON;
-            DEX = DEX;
-            INT = INT;
-            WIS = WIS;
-            CHA = CHA;
+        Player(int str, int con, int dex, int Int, int wis, int cha,
+               double cd, double cc, double hp, double def, double dmg,
+               string origin, string name, double money, int lvl, double xp) {
+            STR = str;
+            CON = con;
+            DEX = dex;
+            INT = Int;
+            WIS = wis;
+            CHA = cha;
 
-            CD = CD;
-            CC = CC;
+            CD = cd;
+            CC = cc;
 
-            HP = HP;
-            DEF = DEF;
-            DMG = DMG;
-            ORIGIN = ORIGIN;
-            NAME = NAME;
-            MONEY = MONEY;
-            LVL = LVL;
-            XP = XP;
+            HP = hp;
+            HP_MAX = HP;
+            DEF = def;
+            DMG = dmg;
+            ORIGIN = origin;
+            NAME = name;
+            MONEY = money;
+            LVL = lvl;
+            XP = xp;
         }
     void displayStats() {
-        cout << "";
+        cout << "\nHP: " << HP;
+        cout << "\nMax HP: " << HP_MAX;
+        cout << "\nDEF: " << DEF;
+        cout << "\nDMG: " << DMG;
+        cout << "\nSTR: " << STR;
+        cout << "\nCON: " << CON;
+        cout << "\nDEX: " << DEX;
+        cout << "\nINT: " << INT;
+        cout << "\nWIS: " << WIS;
+        cout << "\nCHA: " << CHA;
+        cout << "\nCrit Damage: " << CD << "%";
+        cout << "\nCrit Chance: " << CC << "%";
+        cout << "\nOrigin: " << ORIGIN;
+        cout << "\nName: " << NAME;
+        cout << "\nMoney: " << MONEY;
+        cout << "\nLVL: " << LVL;
+        cout << "\nXP: " << XP;
+    }
+
+    void levelUp() {
+        LVL++;
+        XP = 0;
+        cout << "\nYou leveled up to level: " << LVL;
+    }
+
+    void heal(double healAmount) {
+        HP += healAmount;
+        cout << "\nYou healed for " << healAmount << " HP";
+        cout << "\nHP: " << HP << "/" << HP_MAX;
+    }
+
+    void hurt(double damageTaken) {
+        HP -= damageTaken;
+        cout << "\nYou were attacked for " << damageTaken << " HP";
+        cout << "\nHP: " << HP << "/" << HP_MAX;
     }
 };
 
 int main(){
-    //Stats: str, con, dex, int, wis, cha, cd, cc, hp, arm, dmg, origin, name, money, lvl, xp
-    Player player(1, 1, 1, 1, 1, 1, 1.10, 1, 100, 0, 1, "", "", 0, 1, 0);
+    //Stats: str, con, dex, int, wis, cha, cd, cc, hp, def, dmg, origin, name, money, lvl, xp
+    Player player(1, 1, 1, 1, 1, 1, 110, 1, 100, 0, 1, "", "", 0, 1, 0);
 
-
+    player.displayStats();
+    player.levelUp();
+    player.hurt(20);
+    player.heal(10);
 
     return 0;
 }
