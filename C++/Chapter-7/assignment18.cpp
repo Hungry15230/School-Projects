451f8dc2bfb264825a0d59fac23f703cd0cf0040
Hungry15230 <Hungry15230@gmail.com>
2025-04-16 14:08:30 -0400

    I dunno

---
+++ C++/Chapter-7/assignment18.cpp
@@ -0,0 +1,18 @@
+/*
+Create a program that displays the weekly gross pay for any number of employees. The 
+user will input the number of hours the employee worked and the employee’s hourly 
+rate. Employees working more than 40 hours receive time and one-half for the hours 
+worked over 40. If necessary, create a new project named Introductory18 Project, and 
+save it in the Cpp8\Chap07 folder. Enter the C++ instructions into a source file named 
+Introductory18.cpp. Also enter appropriate comments and any additional instructions 
+required by the compiler. Save, run, and test the program.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
